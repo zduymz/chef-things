82e45cfce375a728eeab91e4c83387892847e6dc
Mai Duy <kenvin.md@gmail.com>
2016-07-18 17:18:38 +0700

    metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license 'all_rights'
 description 'Installs/Configures cheftesting'
 long_description 'Installs/Configures cheftesting'
 version '0.1.0'
+
+source_url 'https://github.com/zduymz/chef-things'
+issues_url 'https://github.com/zduymz/chef-things/issues'
+
+chef_version '>= 12.0' if respond_to?(:chef_version)
